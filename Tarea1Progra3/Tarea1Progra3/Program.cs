49e7fd93132aea13f0248726ef192e227537a821
Randsiwim <randsiwin13@gmail.com>
2024-09-22 22:07:42 -0600

    Tarea 1 Progra 3

---
+++ Tarea1Progra3/Tarea1Progra3/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea1Progra3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Cantidad de estudiantes: ");
+            int cantidadEstudiantes = int.Parse(Console.ReadLine());
+
+            List<Estudiante> estudiantes = new List<Estudiante>();
+
+            for (int i = 0; i < cantidadEstudiantes; i++)
+            {
+                Console.WriteLine($"Ingresar datos del estudiante {i + 1}: ");
+
+                Console.Write("Nombre: ");
+                string nombre = Console.ReadLine();
+
+                Console.Write("Edad: ");
+                int edad = int.Parse(Console.ReadLine());
+
+                Console.Write("Promedio: ");
+                double promedio = double.Parse(Console.ReadLine());
+
+                estudiantes.Add(new Estudiante(nombre, edad, promedio));
+            }
+
+            int j = 0;
+            while (j < estudiantes.Count)
+            {
+                Estudiante estudiante = estudiantes[j];
+                if (estudiante.Promedio >= 70)
+                {
+                    estudiante.MostrarInfo();
+                    Console.WriteLine(estudiante.EsMayorDeEdad() ? "Mayor de edad" : "Menor de edad");
+
+                }
+                j++;
+            }
+        }
+    }
+}
